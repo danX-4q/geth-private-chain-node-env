6c5977354164fcb8f9a03ae6159b06b8f3bca1ee
root <root@danx-ubuntu-16.04.5>
2018-09-07 14:58:00 +0800

    service端：1.正确启动rpc服务；2.默认关闭挖矿

--- geth-service.sh
+++ geth-service.sh
@@ -2,7 +2,7 @@
 
 . geth-env.conf
 
-geth $GETH_PARA__datadir $GETH_PARA__ethash_dagdir --nodiscover --mine --miner.threads 4 --rpcaddr 0.0.0.0 2>> $GETH_ENV__logfile &
+geth $GETH_PARA__datadir $GETH_PARA__ethash_dagdir --nodiscover --mine --miner.threads 0 --rpc --rpcaddr 0.0.0.0 --rpcport 8545 --rpcapi "db,eth,net,web3" >> $GETH_ENV__logfile &
 
 echo ""
 echo ""
