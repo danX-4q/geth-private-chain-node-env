2489230161ec693f6475deb35684c0852d6bc449
danX-4q <69082819@qq.com>
2018-09-08 15:28:37 +0800

    增加参数，接入Ethereum钱包和etherparty的区块浏览器；实际验证有效

--- geth-console.sh
+++ geth-console.sh
@@ -2,5 +2,8 @@
 
 . geth-env.conf
 
-geth $GETH_PARA__datadir  $GETH_PARA__ethash_dagdir --nodiscover console 2>> $GETH_ENV__logfile
+geth $GETH_PARA__datadir  $GETH_PARA__ethash_dagdir \
+    --nodiscover --networkid 2018 --maxpeers 5 \
+    --rpccorsdomain "http://192.168.186.30:8000,http://127.0.0.1:8000" \
+    console 2>> $GETH_ENV__logfile
 
--- geth-service.sh
+++ geth-service.sh
@@ -2,7 +2,12 @@
 
 . geth-env.conf
 
-geth $GETH_PARA__datadir $GETH_PARA__ethash_dagdir --nodiscover --mine --miner.threads 0 --rpc --rpcaddr 0.0.0.0 --rpcport 8545 --rpcapi "db,eth,net,web3" >> $GETH_ENV__logfile &
+geth $GETH_PARA__datadir $GETH_PARA__ethash_dagdir \
+    --nodiscover --networkid 2018 --maxpeers 5 \
+    --mine --miner.threads 4 \
+    --rpc --rpcaddr 0.0.0.0 --rpcport 8545 --rpcapi "db,eth,net,web3,personal" \
+    --rpccorsdomain "http://192.168.186.30:8000,http://127.0.0.1:8000" \
+    2>> $GETH_ENV__logfile &
 
 echo ""
 echo ""
