2e6544967dbdaa8152f743f1380fb8f5cd716165
danx <69082819@qq.com>
2019-12-13 19:53:38 +0800

    remove burnable

--- erc20CJX/contracts/TutorialToken.sol
+++ erc20CJX/contracts/TutorialToken.sol
@@ -2,18 +2,24 @@ pragma solidity ^0.5.0;
 
 //https://www.trufflesuite.com/tutorials/robust-smart-contracts-with-openzeppelin
 
-//import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 
-contract TutorialToken is ERC20Mintable,ERC20Burnable {
+contract TutorialToken is ERC20Mintable {
     string public name = "CJXToken";
     string public symbol = "CJX";
     uint8 public decimals = 4;
-    uint public INITIAL_SUPPLY = 10000000000;
+    uint public INITIAL_SUPPLY = 100000000000000;
 
     constructor() public {
         _mint(msg.sender, INITIAL_SUPPLY);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(_msgSender(), amount);
+    }
+
+    function burnFrom(address account, uint256 amount) public {
+        _burnFrom(account, amount);
     }
 
 }
