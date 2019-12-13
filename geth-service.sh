029ca6321ac1bc912796b013c69ae6fd5109bf7c
danx <69082819@qq.com>
2019-12-13 19:42:24 +0800

    save account for next test.

--- geth-service.sh
+++ geth-service.sh
@@ -3,7 +3,7 @@
 . geth-env.conf
 
 geth $GETH_PARA__datadir $GETH_PARA__ethash_dagdir \
-    --nodiscover --networkid 2018 --maxpeers 5 \
+    --nodiscover --networkid 2018 --maxpeers 5 --allow-insecure-unlock --password ./password \
     --mine --miner.threads 4 \
     --rpc --rpcaddr 0.0.0.0 --rpcport 8545 --rpcapi "db,eth,net,web3,personal" \
     --rpccorsdomain "http://192.168.186.30:8000,http://127.0.0.1:8000" \
